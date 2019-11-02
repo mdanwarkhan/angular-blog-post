7c040c1834704ea026458ffa1e0af1ab2d7f663a
mdanwarkhan <anwarkhan772@gmail.comn>
2019-11-02 21:10:41 +0530

    added content-projection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { TruncatePipe } from './truncate.pipe';
 import { PaginatorComponent } from './paginator/paginator.component';
 import { BlogDataService } from './blog-data.service';
 import { HighlightDirective } from './highlight.directive';
+import { CardComponent } from './card/card.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HighlightDirective } from './highlight.directive';
     BlogPostViewComponent,
     TruncatePipe,
     PaginatorComponent,
-    HighlightDirective
+    HighlightDirective,
+    CardComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule
